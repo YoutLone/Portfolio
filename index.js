741beae1c860c25d09cd48b393aa64515c27cd50
yemidada <yemidada@gmail.com>
2023-05-15 15:19:46 +0100

    update index.js file

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const openMenu = document.querySelector('#menu');
 const nav = document.querySelector('nav');
 const navIcon = document.querySelectorAll('.navIcon');
+const logo = document.querySelector('.navbar-logo');
+
 
 openMenu.addEventListener('click', () => {
+  logo.classList.toggle('hidden');
   nav.classList.toggle('open');
   navIcon.forEach((icon) => {
     icon.classList.toggle('hidden');
