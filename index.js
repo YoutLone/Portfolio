7e551eeed450687cdbf6505bc705d3bc58360534
Yout <youtlone95@gmail.com>
2023-05-15 22:06:47 +0630

    update of html css and javascript file

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ const nav = document.querySelector('nav');
 const navIcon = document.querySelectorAll('.navIcon');
 const logo = document.querySelector('.navbar-logo');
 
-
 openMenu.addEventListener('click', () => {
   logo.classList.toggle('hidden');
   nav.classList.toggle('open');
   navIcon.forEach((icon) => {
     icon.classList.toggle('hidden');
-    })
-})
+  });
+});
