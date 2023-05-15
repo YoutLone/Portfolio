4d9e6318abcad42e071038628eb9fe35405f8689
Yout <youtlone95@gmail.com>
2023-05-15 20:56:25 +0630

    update index.js

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const openMenu = document.querySelector('#menu');
 const nav = document.querySelector('nav');
 const navIcon = document.querySelectorAll('.navIcon');
 
-openMenu.addEventListener('click', () => {
-  nav.classList.toggle('open');
-  navIcon.forEach((icon) => {
+
+openMenu.addEventListener("click",() => {
+nav.classList.toggle('open');
+navIcon.forEach(icon => {
     icon.classList.toggle('hidden');
-  });
-});
+    })
+})
