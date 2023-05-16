bd50d86943ee82f453982d21f28179472c631c31
Yout <youtlone95@gmail.com>
2023-05-16 07:09:19 +0630

    changing of index.js and index.html to resize the mobile menu

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const openMenu = document.querySelector('#menu');
 const nav = document.querySelector('nav');
 const navIcon = document.querySelectorAll('.navIcon');
 const logo = document.querySelector('.navbar-logo');
+const hamburger = document.querySelector('#hamburger');
 
 openMenu.addEventListener('click', () => {
   logo.classList.toggle('hidden');
@@ -9,4 +10,15 @@ openMenu.addEventListener('click', () => {
   navIcon.forEach((icon) => {
     icon.classList.toggle('hidden');
   });
+});
+
+window.addEventListener('resize', () => {
+  if (document.body.clientWidth > 768) {
+    nav.classList.remove('open');
+    navIcon.forEach((icon) => {
+      icon.classList.add('hidden');
+    });
+    hamburger.classList.remove('hidden');
+    logo.classList.remove('hidden');
+  }
 });
